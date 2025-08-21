07dc6618b5e15ea7b81850bf594dd174adc4877b
hungvannguyen <nguyenvanhung08012002@gmail.com>
2025-08-21 10:56:01 +0700

    Remove default greeting display and update greeting logic in helloWorld

--- helloWord/helloWord.js
+++ helloWord/helloWord.js
@@ -1,12 +1,13 @@
   let name = 'Hung';
+  let count = 0;
 
   const hello = (function helloWorld() {
       return 'hello, ' + name;
   })();
 
-      document.getElementById('default').innerText = hello;
+      document.getElementById('greeting').innerText = hello;
       console.log(hello);
 
       document.getElementById('hello').addEventListener('click', function() {
-          document.getElementById('greeting').innerText = hello;
+          document.getElementById('greeting').innerText = hello + ' ' + count++;
       });
