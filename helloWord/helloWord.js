91f344ee4ff0f2cd5f530e03a930748965150838
hungvannguyen <nguyenvanhung08012002@gmail.com>
2025-08-21 11:44:13 +0700

    Refactor helloWorld functionality by rearranging elements and updating greeting logic

--- helloWord/helloWord.js
+++ helloWord/helloWord.js
@@ -1,13 +1,16 @@
-let name = 'Hung';
-let count = 0;
+
 let userName = document.getElementById('userName');
+let money = document.getElementById('money');
+let wallet = 1234;
+
+let transactionHistory = [];
 
 function hello() {
-  return 'hello, ' + (userName && userName.value ? userName.value : name);
+  return 'User ' + userName.value + ' send ' + money.value + ' to ' + wallet;
 }
 
-document.getElementById('greeting').innerText = hello();
-
 document.getElementById('showGreeting').addEventListener('click', function () {
+  transactionHistory.push(hello());
   document.getElementById('greeting').innerText = hello();
+  document.getElementById('transHistory').innerHTML = transactionHistory.join('<br>');
 });
